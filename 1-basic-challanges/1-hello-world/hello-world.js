18431806b2da154e393122985651eec8c5074a9b
Jovana Marceta <jmarcetafedev@gmail.com>
2023-10-22 13:28:43 +0200

    Adding first challange and gitignore

---
+++ 1-basic-challanges/1-hello-world/hello-world.js
@@ -0,0 +1,13 @@
+function helloWorld() {
+  return 'Hello World!';
+}
+
+module.exports = helloWorld;
+
+
+
+// function helloWorld() {
+//   return console.log("Hello World");
+// }
+
+// helloWorld();
